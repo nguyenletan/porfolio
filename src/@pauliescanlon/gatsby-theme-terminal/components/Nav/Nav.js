a909492d52f9f7da81abb40ba59e87522e5f77fd
Tan Nguyen Le <nguyenletan@gmail.com>
2020-07-18 23:36:08 +1000

    fixed break build

--- src/@pauliescanlon/gatsby-theme-terminal/components/Nav/Nav.js
+++ src/@pauliescanlon/gatsby-theme-terminal/components/Nav/Nav.js
@@ -4,33 +4,32 @@ import { jsx } from "theme-ui"
 import { MdxRoutes } from "@pauliescanlon/gatsby-mdx-routes"
 import { Link } from "gatsby"
 import "./index.css"
-
 import { Logo } from "../Logo/Logo"
 import ThemeSwitch from "./themeswitch"
 
-import * as styles from "@pauliescanlon/gatsby-theme-terminal/src/components/Nav/styles"
+import {logo, nav, ul, li, link} from "@pauliescanlon/gatsby-theme-terminal/src/components/Nav/styles"
 
 const DUMMY = "dummy"
 
 export const Nav = () => (
   <Fragment>
-    <div sx={styles.logo}>
+    <div sx={logo}>
       <Logo />
     </div>
     <div></div>
-    <nav sx={styles.nav}>
+    <nav sx={nav}>
       <MdxRoutes>
         {(routes, _) => (
-          <ul sx={styles.ul}>
+          <ul sx={ul}>
             {routes
               .filter(
                 route =>
                   route.navigationLabel && route.navigationLabel !== DUMMY
               )
               .map((route, index) => (
-                <li sx={styles.li} key={index}>
+                <li sx={li} key={index}>
                   <Link
-                    sx={styles.link}
+                    sx={link}
                     activeClassName="active-link"
                     to={route.slug}
                   >
@@ -38,7 +37,7 @@ export const Nav = () => (
                   </Link>
                 </li>
               ))}
-            <li sx={styles.li}>
+            <li sx={li}>
               <ThemeSwitch />
             </li>
           </ul>
