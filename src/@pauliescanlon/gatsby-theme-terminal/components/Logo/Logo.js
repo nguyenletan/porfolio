7ed899e5f56d33a1fa8eae561b386361797855db
Tan Nguyen Le <nguyenletan@gmail.com>
2020-07-19 13:57:27 +1000

    fix ui bug on Mobile

--- src/@pauliescanlon/gatsby-theme-terminal/components/Logo/Logo.js
+++ src/@pauliescanlon/gatsby-theme-terminal/components/Logo/Logo.js
@@ -3,8 +3,9 @@ import { jsx } from "theme-ui"
 import styled from "styled-components"
 
 const H2 = styled("h2")`
-  width: 200px;
+  width: 280px;
   font-weight: 700;
+  text-align: right;
 `
 
 export const Logo = () => <H2 sx={{ color: "success" }}>Tan Nguyen LE</H2>
